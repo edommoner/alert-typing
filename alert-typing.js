394333010370aa6619174bd097f55a5aa742f717
edo <edoofbone@gmail.com>
2019-04-01 15:20:41 +0900

    no message

--- alert-typing.js
+++ alert-typing.js
@@ -92,28 +92,6 @@ async function typing(message_ary = [], glitch_flag2 = false) {
 }
 
 function glitch2() {
-    $("#alert-text-wrap")
-        .append($("<div id='decoration-wrap' class='decoration-wrap'/>")
-            .append(
-                $("<div id='decoration-box'/>")
-                .append($("<div id='decoration-text-bak'/>"))
-                .append($("<div id='decoration-text-area'/>"))
-            )
-            .append($("<div class='decoration-ring-partial'/>")
-
-            )
-            .append(
-                $("<div id='decoration-ring-wrap'/>")
-                .append($("<div id='decoration-ring1' class='decoration-ring'/>"))
-                .append($("<div id='decoration-ring3' class='decoration-ring2'/>"))
-            )
-        );
-
-    $("#alert-text").css({ "padding": "0", "margin": "0" });
-    $("#alert-message").css({ "font-size": "", "font-weight": "" });
-    $("#alert-user-message").css({ "font-size": "", "font-weight": "" });
-    $("#decoration-text-area").append($("#alert-text"));
-
     $(".decoration-wrap").mgGlitch({
         // set 'true' to stop the plugin
         destroy: false,
