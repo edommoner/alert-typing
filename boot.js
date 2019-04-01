b759f49399e060fae2b840e9107f2c7a4a3090d8
edo <edoofbone@gmail.com>
2019-04-01 19:47:51 +0900

    no message

--- boot.js
+++ boot.js
@@ -1,4 +1,20 @@
-$(window).on("load", function() {
+$(function() {
+    url_common = "//cdn.jsdelivr.net/gh/edommoner/alert-typing@";
+    url_ver = "0.3.7.5/";
+    url_head = url_common + url_ver;
+
+    loadCss(url_head + "lame.min.css",
+        loadCss(url_head + "alert-typing.min.css",
+            loadScript(url_head + "mgGlitch.min.js",
+                loadScript(url_head + "flame.min.js",
+                    loadScript(url_head + "alert-typing.min.js", start()))
+            )
+        )
+    );
+});
+
+function start() {
+
     let data_effect = $("#alert-text-wrap").attr("data-effect");
     let data_flame = $("#alert-text-wrap").attr("data-flame");
 
@@ -43,4 +59,47 @@ $(window).on("load", function() {
 
 
     $().alert_typing();
-});
+}
+
+function loadScript(src, callback) {
+    var done = false;
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.src = src;
+    head.appendChild(script);
+    // Attach handlers for all browsers
+    script.onload = script.onreadystatechange = function() {
+        if (!done && (!this.readyState ||
+                this.readyState === "loaded" || this.readyState === "complete")) {
+            done = true;
+            callback();
+            // Handle memory leak in IE
+            script.onload = script.onreadystatechange = null;
+            if (head && script.parentNode) {
+                head.removeChild(script);
+            }
+        }
+    };
+}
+
+function loadCss(src, callback) {
+    var done = false;
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    head.appendChild(link);
+    link.href = src;
+    // Attach handlers for all browsers
+    link.onload = link.onreadystatechange = function() {
+        if (!done && (!this.readyState ||
+                this.readyState === "loaded" || this.readyState === "complete")) {
+            done = true;
+            callback();
+            // Handle memory leak in IE
+            link.onload = link.onreadystatechange = null;
+            if (head && link.parentNode) {
+                head.removeChild(link);
+            }
+        }
+    };
+}
