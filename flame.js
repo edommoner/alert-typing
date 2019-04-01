69f78f924001adbc63f325ce66942b2e46dbb8df
edo <edoofbone@gmail.com>
2019-04-01 14:50:47 +0900

    no message

--- flame.js
+++ flame.js
@@ -1,18 +1,19 @@
 $(function() {
 
     $("#alert-text-wrap")
-        .append($("<div id='decoration-wrap'/>")
+        .append($("<div id='decoration-wrap' class='decoration-wrap'/>")
             .append(
                 $("<div id='decoration-box'/>")
                 .append($("<div id='decoration-text-bak'/>"))
                 .append($("<div id='decoration-text-area'/>"))
             )
+            .append($("<div id='decoration-ring-partial'/>")
+
+            )
             .append(
                 $("<div id='decoration-ring-wrap'/>")
                 .append($("<div id='decoration-ring1' class='decoration-ring'/>"))
-                .append($("<div id='decoration-ring2' class='decoration-ring'/>"))
                 .append($("<div id='decoration-ring3' class='decoration-ring2'/>"))
-                .append($("<div id='decoration-ring4' class='decoration-ring2'/>"))
             )
         );
 
